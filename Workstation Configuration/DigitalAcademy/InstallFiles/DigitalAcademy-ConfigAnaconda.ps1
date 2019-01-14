64e0df01eb82d2983174a3681c72cdbc436eb9e8
Cory Dignard <cory.dignard@gmail.com>
2019-01-14 15:05:04 -0500

    Initial files
    Initial files

---
+++ Workstation Configuration/DigitalAcademy/InstallFiles/DigitalAcademy-ConfigAnaconda.ps1
@@ -0,0 +1,118 @@
+﻿#requires -version 4
+#Requires -RunAsAdministrator
+
+<#
+.SYNOPSIS
+  Configure Anaconda
+
+.DESCRIPTION
+  Updates: pip (python), installs PyHamcrest (python), Anaconda, Anaconda Navigator, pip (Anaconda)
+  Adds new channels to Anaconda
+  Creates a new environment named Ferret with python 3.7 and R
+  Installs: RStudio,vnew python and R packages into Ferret environment
+  
+.PARAMETER InstallFiles
+  Folder where installation files are
+
+.INPUTS
+  None
+
+.OUTPUTS Log File
+  The script log file stored in $InstallFiles\DigitalAcademy-ConfigAnaconda.log
+
+.NOTES
+  Version:        1.0
+  Author:         Cory Dignard
+  Creation Date:  December 18, 2018
+  Purpose/Change: Initial script development
+
+  Version:        2.0
+  Author:         Cory Dignard
+  Creation Date:  January 13, 2018
+  Purpose/Change: Refined code
+
+.EXAMPLE
+  C:\DigitalAcademy\InstallFiles\DigitalAcademy-ConfigAnaconda.ps1 -InstallFiles "C:\DigitalAcademy\InstallFiles"
+  
+#>
+
+#---------------------------------------------------------[Script Parameters]------------------------------------------------------
+
+Param (
+  [Parameter(Mandatory=$true)][string]$InstallFiles
+)
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+
+#Set Error Action to Silently Continue
+$ErrorActionPreference = 'SilentlyContinue'
+
+#Import Modules & Snap-ins
+Import-Module PSLogging
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+
+#Script Version
+$sScriptVersion = '2.0'
+
+#Log File Info
+$sLogPath = $InstallFiles
+$sLogName = 'DigitalAcademy-ConfigAnaconda.log'
+$sLogFile = Join-Path -Path $sLogPath -ChildPath $sLogName
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+Function Write-Message ($message) {
+    write-host "`n$message" -ForegroundColor Green;write-host ""
+    Write-LogInfo -LogPath $sLogFile -Message "`n$message"
+
+}
+
+Function InstallSoftware ($Msg,$InstallCommands,$Arguments) {
+
+  Begin {
+    Write-Message "$Msg"
+  }
+
+  Process {
+    Try {
+      Start-Process "$InstallCommands" -ArgumentList "$Arguments" -Wait -NoNewWindow
+    }
+
+    Catch {
+      Write-LogError -LogPath $sLogFile -Message $_.Exception -ExitGracefully
+      Break
+    }
+  }
+
+  End {
+    If ($?) {
+      write-Message "Completed."
+    }
+  }
+}
+
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
+Start-Log -LogPath $sLogPath -LogName $sLogName -ScriptVersion $sScriptVersion | Out-Null
+Write-Host "";"Log file: $sLogFile";Write-Host ""
+
+InstallSoftware "Updating pip (python)" "python" "-m pip install --upgrade pip"
+InstallSoftware "Installing PyHamcrest (python)" "pip" "install PyHamcrest"
+InstallSoftware "Updating Anaconda" "conda" "update conda -y"
+InstallSoftware "Updating Anaconda Navigator" "conda" "update anaconda-navigator -y"
+InstallSoftware "Updating pip (Anaconda)" "conda" "update pip -y"
+foreach($_ in Get-Content $InstallFiles\Anaconda-Channels.txt) {
+    InstallSoftware "Adding channel $_" "conda" "config --append channels $_"
+}
+
+InstallSoftware "Creating environment named Ferret, this will take a while" "conda" "create -n Ferret python=3.7 r-base r-essentials -y"
+
+InstallSoftware "Installing RStudio, this will take a while" "conda" "install -n Ferret -c r rstudio -y"
+
+InstallSoftware "Downloading and installing new packages, this will take a while" "conda" "install -n Ferret --file $InstallFiles\NewPackages.txt -y"
+
+Write-Message "All tasks completed."
+
+Stop-Log -LogPath $sLogFile
---
+++ Workstation Configuration/DigitalAcademy/InstallFiles/DigitalAcademy-Installation.ps1
@@ -0,0 +1,149 @@
+﻿#requires -version 4
+#Requires -RunAsAdministrator
+
+<#
+.SYNOPSIS
+  Installs and configures everything needed for Digital Academy Premium Data & AI courses
+
+.DESCRIPTION
+  Sets: Power Plan, Time Zone, Desktop Wallpaper 
+  Installs: Anaconda, R, RStudio, sqlite, SQLite Studio, Power BI Desktop, Chrome, Slack Desktop,
+  Github Desktop, Desktop shortcuts 
+  Adds Path Environment variables 
+  Disables Power BI Registration Sceeen, Slack Updater
+
+.PARAMETER InstallFiles
+  Folder where installation files are
+ 
+.INPUTS
+  None
+
+.OUTPUTS Log File
+  The script log file stored in $InstallFiles\DigitalAcademy-Installation.log
+
+.NOTES
+  Version:        1.0
+  Author:         Cory Dignard
+  Creation Date:  December 18, 2018
+  Purpose/Change: Initial script development
+
+  Version:        2.0
+  Author:         Cory Dignard
+  Creation Date:  January 13, 2018
+  Purpose/Change: Added new requirements and refined code
+
+.EXAMPLE
+  C:\DigitalAcademy\InstallFiles\DigitalAcademy-Installation.ps1 -InstallFiles "C:\DigitalAcademy\InstallFiles"
+  
+#>
+
+#---------------------------------------------------------[Script Parameters]------------------------------------------------------
+
+Param (
+  [Parameter(Mandatory=$true)][string]$InstallFiles
+)
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+
+#Set Error Action to Silently Continue
+$ErrorActionPreference = 'SilentlyContinue'
+
+#Import Modules & Snap-ins
+Import-Module PSLogging
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+
+#Script Version
+$sScriptVersion = '2.0'
+
+#Log File Info
+$sLogPath = $InstallFiles
+$sLogName = 'DigitalAcademy-Installation.log'
+$sLogFile = Join-Path -Path $sLogPath -ChildPath $sLogName
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+Function Write-Message ($message) {
+    write-host "`n$message" -ForegroundColor Green;write-host ""
+    Write-LogInfo -LogPath $sLogFile -Message "`n$message"
+
+}
+
+Function InstallSoftware ($Software,$InstallCommands,$Arguments) {
+
+  Begin {
+    Write-Message "Installing $Software"
+  }
+
+  Process {
+    Try {
+      Start-Process "$InstallCommands" -ArgumentList "$Arguments" -Wait -NoNewWindow
+    }
+
+    Catch {
+      Write-LogError -LogPath $sLogFile -Message $_.Exception -ExitGracefully
+      Break
+    }
+  }
+
+  End {
+    If ($?) {
+      write-Message "Completed."
+    }
+  }
+}
+
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
+Start-Log -LogPath $sLogPath -LogName $sLogName -ScriptVersion $sScriptVersion | Out-Null
+Write-Host "";"Log file: $sLogFile";Write-Host ""
+
+Write-Message "Setting up Power Plan"
+powercfg /import "$InstallFiles\PowerPlan.pow"
+$HighPerf = powercfg -l | %{if($_.contains("POWER")) {$_.split()[3]}}
+powercfg -setactive $HighPerf
+Write-Message "Completed"
+
+Write-Message "Set Time Zone"
+Set-TimeZone -Id "Eastern Standard Time"
+Write-Message "Completed"
+
+Write-Message "Installing Applications"
+InstallSoftware "Anaconda" "$InstallFiles\Anaconda3-5.3.1-Windows-x86_64.exe" "/InstallationType=AllUsers /RegisterPython=1 /S"
+InstallSoftware "R" "$InstallFiles\R-3.5.1-win.exe" "/VERYSILENT"
+InstallSoftware "RStudio" "$InstallFiles\RStudio-1.1.463" "/S"
+InstallSoftware "sqlite" "xcopy" "$InstallFiles\sqlite C:\ProgramData\sqlite /I /S /Y /Q"
+InstallSoftware "SQLite Studio" "xcopy" "$InstallFiles\SQLiteStudio C:\ProgramData\SQLiteStudio /I /S /Y /Q"
+InstallSoftware "PowerBI Desktop" "msiexec.exe" "/i $InstallFiles\PBIDesktop_x64.msi ACCEPT_EULA=1 /q"
+InstallSoftware "Chrome" "msiexec" "/i $InstallFiles\GoogleChromeStandaloneEnterprise64.msi /q"
+InstallSoftware "Slack" "$InstallFiles\SlackSetup.exe" "/silent"
+InstallSoftware "Github" "$InstallFiles\GitHubDesktopSetup" "/silent"
+InstallSoftware "Desktop Shortcuts" "xcopy" "$InstallFiles\*.lnk C:\Users\Public\Desktop /Y /Q"
+Write-Message "Completed"
+
+Write-Message "Set Desktop Wallpaper"
+Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value "$InstallFiles\shutterstock_100185233.jpg"
+Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name WallpaperStyle -value "6"
+rundll32.exe user32.dll, UpdatePerUserSystemParameters
+Write-Message "Completed"
+
+Write-Message "Adding paths"
+$oldPath = [System.Environment]::GetEnvironmentVariable('Path', 'Machine')
+$newPath = $oldPath + ";C:\ProgramData\sqlite;C:\ProgramData\SQLiteStudio;C:\ProgramData\Anaconda3;C:\ProgramData\Anaconda3\Scripts"
+[System.Environment]::SetEnvironmentVariable('Path', $newPath, [System.EnvironmentVariableTarget]::Machine)
+Write-Message "Completed"
+
+Write-Message "Disable Power BI Registration Screen"
+Set-ItemProperty 'HKCU:\Software\Microsoft\Microsoft Power BI Desktop' -Name ShowLeadGenDialog -Value "0"
+rundll32.exe user32.dll, UpdatePerUserSystemParameters
+Write-Message "Completed"
+
+Write-Message "Disable Slack Updater"
+Set-ItemProperty HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run -Name com.squirrel.slack.slack -Value ([byte[]](0x03,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00))
+rundll32.exe user32.dll, UpdatePerUserSystemParameters
+Write-Message "Completed"
+
+Write-Message "All tasks completed."
+
+Stop-Log -LogPath $sLogFile
